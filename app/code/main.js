f2965804fb0b5464b5561338a5bb98c5b1042fa2
konstaintine811 <galystyan777@gmail.com>
2017-07-20 14:21:42 +0300

    fix bugs and add scrollOverflow

--- app/code/main.js
+++ app/code/main.js
@@ -2,11 +2,16 @@
 * @Author: Admin
 * @Date:   2017-07-19 19:43:56
 * @Last Modified by:   Admin
-* @Last Modified time: 2017-07-19 19:49:16
+* @Last Modified time: 2017-07-20 14:14:46
 */
 
 'use strict';
 
 $(document).ready(function() {
-	$('#fullpage').fullpage();
+	$('#fullpage').fullpage({
+		sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+		anchors: ['firstPage', 'secondPage', 'thirdPage'],
+		menu: '#menu',
+		scrollOverflow: true,
+	});
 });
